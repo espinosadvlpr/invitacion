28703d044ed93d3aaeb14e001bd15917b64d228d
David Espinosa <espinosadavid47@gmail.com>
2022-12-24 15:43:46 -0500

    configure gh pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import image from './memeface.png';
+import pdf from './pao.pdf';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <h1>INVITACIÓN VACACIONES</h1>
+      <p>Buenas noches, </p>
+      <p>La presente es para informar que la señorita Paula Valeria Sanchez Nieto ha sido invitada a un viaje en ENERO 2023</p>
+      <p>Lugar de salida: Tunja</p>
+      <p>Lugar de destino: Neiva</p>
+      <p>Fecha: ENERO 2023</p>
+      <p>Vehiculo: a pie</p>
+      <button className="button">
+        <a href={pdf} target="_blank" rel="noopener noreferrer" download="invitacio_pao.pdf"> 
+        Descargue su ticket
         </a>
-      </header>
+      </button>
+      <p>Muchas gracias, quedo atento a su respuesta.</p>
+      <img src={image}/>
     </div>
   );
 }
